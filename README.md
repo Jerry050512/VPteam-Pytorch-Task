54f8a8d18802fe561939b3bac4dc786de6eecc24
Gang <jerry050512@outlook.com>
2024-04-14 21:56:55 +0800

    Add replace rule

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 ## VPTeam 的任务解答
 
+本仓库中包含了VPTeam 的任务解答，请运行：
+```cmd
+pip install -r requirements.txt
+```
+来安装所需的依赖。
+
 ### Task-1 手写数字识别
 
 `written-number-predict.ipynb` 实现了对MNIST的训练，并测试了`./test/mnist`中的图片。
 
+### Task-2 显著性图片生成
+
+`salient-object-detection.ipynb` 实现了显著性图片生成，
+并测试了`./test/recaptcha/pictures`中的图片。
--- test/recaptcha/captcha_gen.py
+++ test/recaptcha/captcha_gen.py
@@ -3,11 +3,17 @@ import random
 import string
 import os
 
+replace_rule = {
+    "O": "0",
+    "1": "I",
+    "Z": "2",
+}
+
 # 创建一个ImageCaptcha实例
 image = ImageCaptcha(width=280, height=90)
 
 # 指定保存验证码图片的目录
-output_dir = 'pictures'
+output_dir = './test/recaptcha/pictures'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
@@ -17,6 +23,8 @@ with open(os.path.join(output_dir, 'filename.lst'), 'w') as f:
     for i in range(100):
         # 生成随机的四位验证码
         captcha_text = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+        for k, v in replace_rule.items():
+            captcha_text = captcha_text.replace(k, v)
         # 生成验证码图片
         data = image.generate(captcha_text)
         # 构建图片文件名
